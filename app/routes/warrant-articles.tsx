78803ee65252601ed8800a76a04692c38ef11cdd
Elizabeth Schafer <1136332+eschafer@users.noreply.github.com>
2024-02-14 12:22:20 -0500

    Design updates, fixed typos

--- app/routes/warrant-articles.tsx
+++ app/routes/warrant-articles.tsx
@@ -3,27 +3,10 @@ import { useLoaderData } from '@remix-run/react';
 
 import { useState } from 'react';
 import Typography from '@mui/material/Typography';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import TablePagination from '@mui/material/TablePagination';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import { fetchGraphQL } from '../utils';
-
-const columns = [
-  { id: 'session', label: 'Session' },
-  { id: 'number', label: 'Article Number' },
-  { id: 'title', label: 'Article Title' },
-  // { id: 'description', label: 'Article Description' },
-];
-
-function createData(number, title, description, { startDate, sessionName }) {
-  const session = `${startDate} ${sessionName}`;
-  return { session, number, title, description };
-}
 
 export const loader = async ({ request }: { request: Request }) => {
   const query = `
@@ -49,12 +32,12 @@ export const loader = async ({ request }: { request: Request }) => {
   const data = await fetchGraphQL({ query, request });
 
   const rows = data.allWarrantArticles.map((article) => {
-    return createData(
-      `${article.townMeetingSession.sessionName} WA-${article.articleNumber}`,
-      article.articleTitle,
-      article.articleDescription,
-      article.townMeetingSession,
-    );
+    return {
+      session: article.townMeetingSession.startDate,
+      number: article.articleNumber,
+      title: article.articleTitle,
+      description: article.articleDescription,
+    };
   });
 
   return json(rows);
@@ -80,54 +63,36 @@ export default function WarrantArticles() {
       <Typography variant="h1" gutterBottom>
         Warrant Articles
       </Typography>
-      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-        <TableContainer>
-          <Table size="small">
-            <TableHead>
-              <TableRow>
-                {columns.map((column) => (
-                  <TableCell
-                    key={column.id}
-                    align={column.align}
-                    style={{ minWidth: column.minWidth }}
-                  >
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
-                  return (
-                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                      {columns.map((column) => {
-                        const value = row[column.id];
-                        return (
-                          <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === 'number'
-                              ? column.format(value)
-                              : value}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[50, 100]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
+
+      <Grid
+        component="ul"
+        container
+        spacing={3}
+        sx={{ margin: 0, padding: 0, width: '100%', listStyle: 'none' }}
+      >
+        {rows.map((item, index) => (
+          <Grid
+            component="li"
+            item
+            xs={12}
+            key={index}
+            sx={{ paddingLeft: '0 !important' }}
+          >
+            <Card>
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  {item.session} WA-{item.number}
+                  <br />
+                  {item.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {item.description}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }

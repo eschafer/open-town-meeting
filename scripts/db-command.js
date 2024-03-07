7566798fefda1c0c5e632172c9a9facbbcb6b713
Elizabeth Schafer <1136332+eschafer@users.noreply.github.com>
2024-03-07 10:34:57 -0500

    Updated db scripts to support local/remote

--- scripts/db-command.js
+++ scripts/db-command.js
@@ -1,5 +1,9 @@
 import dotenv from 'dotenv';
 import { exec } from 'child_process';
+import arg from 'arg';
+import readline from 'readline';
+
+dotenv.config();
 
 if (!process.env.npm_lifecycle_event) {
   // Make sure this script isn't used elsewhere since it's susceptible to command injection
@@ -9,27 +13,66 @@ if (!process.env.npm_lifecycle_event) {
   process.exit(1);
 }
 
-dotenv.config();
+const argSpec = {
+  '--location': String,
+  '--command': String,
+};
 
-const command = `wrangler d1 execute ${process.env.DATABASE_NAME} --local --command "${process.argv[2]}"`;
+const args = arg(argSpec);
 
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
+// get the env and command values
+const inputLocation = args['--location'] || 'local'; // can be 'local' or 'remote'
+const inputCommand = args['--command'];
 
-  // Remove the non-JSON content at the beginning of the string
-  const jsonStart = stdout.indexOf('[');
-  const jsonEnd = stdout.lastIndexOf(']') + 1; // Include the closing bracket
-  const jsonString = stdout.slice(jsonStart, jsonEnd);
+if (inputLocation === 'remote') {
+  // Create a readline interface
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-  // Parse the JSON string into an array
-  const data = JSON.parse(jsonString);
+  // Ask for confirmation
+  rl.question(
+    '⚠️ WARNING: Are you sure you want to run this command on the remote database? This will affect the production database.\nType "yes" to continue:\n',
+    (answer) => {
+      if (answer === 'yes') {
+        runCommand({ location: inputLocation, command: inputCommand });
+      } else {
+        console.log('Command not confirmed. Exiting.');
+        process.exit(0);
+      }
 
-  console.table(data[0].results);
-});
+      rl.close();
+    },
+  );
+} else if (inputLocation === 'local') {
+  runCommand({ location: inputLocation, command: inputCommand });
+} else {
+  console.error('Invalid location. Use "local" or "remote".');
+  process.exit(1);
+}
+
+function runCommand(params) {
+  const command = `wrangler d1 execute ${process.env.DATABASE_NAME} ${params.location === 'remote' ? '' : '--local'} --command "${params.command}"`;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+
+    // Remove the non-JSON content at the beginning of the string
+    const jsonStart = stdout.indexOf('[');
+    const jsonEnd = stdout.lastIndexOf(']') + 1; // Include the closing bracket
+    const jsonString = stdout.slice(jsonStart, jsonEnd);
+
+    // Parse the JSON string into an array
+    const data = JSON.parse(jsonString);
+
+    console.table(data[0].results);
+  });
+}
--- scripts/db-file.js
+++ scripts/db-file.js
@@ -1,5 +1,9 @@
 import dotenv from 'dotenv';
 import { exec } from 'child_process';
+import arg from 'arg';
+import readline from 'readline';
+
+dotenv.config();
 
 if (!process.env.npm_lifecycle_event) {
   // Make sure this script isn't used elsewhere since it's susceptible to command injection
@@ -9,27 +13,66 @@ if (!process.env.npm_lifecycle_event) {
   process.exit(1);
 }
 
-dotenv.config();
+const argSpec = {
+  '--location': String,
+  '--file': String,
+};
 
-const command = `wrangler d1 execute ${process.env.DATABASE_NAME} --local --file "${process.argv[2]}"`;
+const args = arg(argSpec);
 
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
+// get the env and file values
+const inputLocation = args['--location'] || 'local'; // can be 'local' or 'remote'
+const inputFile = args['--file'];
 
-  // Remove the non-JSON content at the beginning of the string
-  const jsonStart = stdout.indexOf('[');
-  const jsonEnd = stdout.lastIndexOf(']') + 1; // Include the closing bracket
-  const jsonString = stdout.slice(jsonStart, jsonEnd);
+if (inputLocation === 'remote') {
+  // Create a readline interface
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-  // Parse the JSON string into an array
-  const data = JSON.parse(jsonString);
+  // Ask for confirmation
+  rl.question(
+    '⚠️ WARNING: Are you sure you want to run this command on the remote database? This will affect the production database.\nType "yes" to continue:\n',
+    (answer) => {
+      if (answer === 'yes') {
+        runCommand({ location: inputLocation, file: inputFile });
+      } else {
+        console.log('Command not confirmed. Exiting.');
+        process.exit(0);
+      }
 
-  console.table(data[0].results);
-});
+      rl.close();
+    },
+  );
+} else if (inputLocation === 'local') {
+  runCommand({ location: inputLocation, file: inputFile });
+} else {
+  console.error('Invalid location. Use "local" or "remote".');
+  process.exit(1);
+}
+
+function runCommand(params) {
+  const command = `wrangler d1 execute ${process.env.DATABASE_NAME} ${params.location === 'remote' ? '' : '--local'} --file "${params.file}"`;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+
+    // Remove the non-JSON content at the beginning of the string
+    const jsonStart = stdout.indexOf('[');
+    const jsonEnd = stdout.lastIndexOf(']') + 1; // Include the closing bracket
+    const jsonString = stdout.slice(jsonStart, jsonEnd);
+
+    // Parse the JSON string into an array
+    const data = JSON.parse(jsonString);
+
+    console.table(data[0].results);
+  });
+}
